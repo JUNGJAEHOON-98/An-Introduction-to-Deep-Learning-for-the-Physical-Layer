12692a1dc82570482ab0a6974907bb95c9c372f9
JUNGJAEHOON <tree34005@gmail.com>
2022-07-25 17:05:28 +0900

    commit

--- models/autoencoder.py
+++ models/autoencoder.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import numpy as np
 from keras.models import Model
-from keras.layers import Dense, GaussianNoise, BatchNormalization
+from keras.layers import Dense, BatchNormalization
 from keras import Sequential
 from tensorflow.keras.utils import to_categorical
 
@@ -9,24 +9,26 @@ class AutoEncoder(Model):
     def __init__(self, M, num_channels,  rate, Eb_N0):
         super(AutoEncoder, self).__init__()
         self.variance = np.sqrt(1/(2*rate*Eb_N0))
+        self.n_channels = num_channels
 
         self.encoder = Sequential([
             Dense(M, activation='relu', name="encoder_layer1"),
             Dense(num_channels, activation='linear', name="encoder_layer2"),
-            BatchNormalization()  
         ])
-
-        self.channel = GaussianNoise(self.variance)
 
         self.decoder = Sequential([
             Dense(M, activation='relu', name="decoder_layer1"),
-            Dense(M, activation='softmax', name="decoder_layer2")
+            Dense(M, activation='softmax', name="decoder_layer2"),
         ])
 
     def call(self, inputs):
         one_hat_vector = to_categorical(inputs, num_classes=16)
         transmitted_signal = self.encoder(one_hat_vector)
-        channel_output = self.channel(transmitted_signal)
+        transmitted_signal = np.sqrt(self.n_channels)*tf.math.l2_normalize(transmitted_signal, axis=-1)
+
+        noise = self.variance * np.random.randn(transmitted_signal.shape[0], self.n_channels)
+        channel_output = transmitted_signal + noise
+
         output = self.decoder(channel_output)
 
         return output
--- models/main.py
+++ models/main.py
@@ -5,15 +5,19 @@ from tensorflow.keras.utils import to_categorical
 from autoencoder import AutoEncoder
 import matplotlib.ticker as mticker
 import matplotlib.pyplot as plt
-from tqdm import tqdm
 # from keras.optimizers import SGD
 
+def set_seed(seed: int = 25):
+    np.random.seed(seed)
+    tf.random.set_seed(seed)
+
+set_seed(1024)
 
 # defining system parameters
-M = 16 
+M = 16
 k = int(np.log2(M))
-rate = 4/7
 n_channel = 7
+rate = k/n_channel
 EbNo_train = 5.01187 #  coverted 7 db of EbNo
 
 print('-----------SYSTEM PARAMETER-------------')
@@ -25,29 +29,29 @@ print('Eb_No[dB]',)
 print('---------------------------------------\n')
 
 # defining training parameters 
-epochs = 1
-batch_size = 20
-N_train = 10000
-N_val = 5000
+epochs = 20
+batch_size = 256
+lr = 0.002
+N_train = 70000
+N_val = 20000
 
-optimizer = tf.keras.optimizers.Adam() # optimizer = SGD()
+optimizer = tf.keras.optimizers.Adam(learning_rate= lr) # optimizer = SGD()
 loss_fn = tf.keras.losses.CategoricalCrossentropy()
 
 print('----------TRAINING PARAMETER------------')
 print ('epochs:', epochs)
 print ('batch_size', batch_size)
-print('Optimizer:', 'Adam')
-print('Loss function:', 'CategoricalCrossentropy')
+print('Optimizer:', optimizer)
+print('Loss function:',loss_fn )
 print('---------------------------------------\n\n')
 
 #generating data of size N
 
 train_data = np.random.randint(M, size=N_train)
-train_label = to_categorical(train_data)
-train_data = tf.data.Dataset.from_tensor_slices((train_data, train_label)).batch(batch_size)
+train_label = to_categorical(train_data, num_classes=16)
 
 val_data = np.random.randint(M, size=N_val)
-val_label = to_categorical(val_data)
+val_label = to_categorical(val_data, num_classes=16)
 
 
 autoencoder = AutoEncoder(M, n_channel, rate, EbNo_train)
@@ -59,109 +63,106 @@ val_loss_metrics = tf.keras.metrics.Mean()
 val_acc_metrics = tf.keras.metrics.CategoricalAccuracy()
 
 
-def apply_gradient(model, optimizer, loss_fn, x, y):
+def train_step(x, y):
     with tf.GradientTape() as tape:
         predictions = autoencoder(x)
         loss_value = loss_fn(y, predictions)
-    gradients = tape.gradient(loss_value, model.trainable_weights)
-    optimizer.apply_gradients(zip(gradients, model.trainable_weights))
+    gradients = tape.gradient(loss_value, autoencoder.trainable_weights)
+    optimizer.apply_gradients(zip(gradients, autoencoder.trainable_weights))
  
-    return predictions, loss_value
+    train_loss_metrics(loss_value)
+    train_acc_metrics(y, predictions)
 
-
-def train_for_one_epoch(train_data, model, optimizer, loss_fn, train_acc_metrics):
-
-    one_epoch_loss = []
-    one_epoch_acc = []
-
-    for epoch, (x_batch, y_batch) in enumerate(train_data):
-        predictions, loss_value = apply_gradient(model, optimizer, loss_fn, x_batch, y_batch)       
-       
- 
-        one_epoch_loss.append(train_loss_metrics(loss_value))
-        one_epoch_acc.append(train_acc_metrics(y_batch, predictions))
-
-    return np.mean(one_epoch_loss), np.mean(one_epoch_acc)
-
-    
-
-def perform_validation(test_data, test_label, model, loss_fn, valid_acc_metrics):   
-    predictions = model(test_data)
-
+def valid_step(test_data, test_label):   
+    predictions = autoencoder(test_data)
     loss_value = loss_fn(test_label, predictions)
-    acc_value = valid_acc_metrics(test_label, predictions)
-
-    return loss_value, acc_value
-    
-
-val_losses, train_losses = [], []
+    val_loss_metrics(loss_value)
+    val_acc_metrics(test_label, predictions)
 
 for epoch in range(epochs):
-    print(f'Start of epoch {epoch+1}')
-  
-    train_loss, train_acc = train_for_one_epoch(train_data, autoencoder, optimizer, loss_fn, train_acc_metrics)
-    val_loss, val_acc = perform_validation(val_data, val_label, autoencoder, loss_fn, val_acc_metrics)
-  
+
+    for i in range(N_train // batch_size):
+        x_batch = train_data[i*batch_size:(i+1)*batch_size]
+        y_batch = train_label[i*batch_size:(i+1)*batch_size]
+        train_step(x_batch, y_batch)
+
+    valid_step(val_data, val_label)
     template = 'epoch: {}, train_loss: {:.5f}, train_acc: {:.2f}%, val_loss: {:.5f}, val_acc: {:.2f}%'
-
-    print (template.format(epoch+1,
-                        train_loss,
-                        train_acc*100,
-                        val_loss,
-                        val_acc*100))
-
-    train_losses.append(train_loss_metrics.result())
-    val_losses.append(val_loss_metrics.result())
-
-
-def plot_metrics(train_metric, valid_metric, title):
-    plt.title(title)
-    plt.gca().xaxis.set_major_locator(mticker.MultipleLocator(1))
-    plt.plot(train_metric, color='red', label="train_loss")
-    plt.plot(valid_metric, color='blue', label= "val_loss")
-    plt.legend()
- 
-plot_metrics(train_losses, val_losses, "Loss Value")
-
-N = 45000
-test_label = np.random.randint(M,size=N)
-test_data = []
-
-for i in test_label:
-    temp = np.zeros(M)
-    temp[i] = 1
-    test_data.append(temp)
     
-test_data = np.array(test_data)
+    print (template.format(epoch+1,
+                        train_loss_metrics.result(),
+                        train_acc_metrics.result()*100,
+                        val_loss_metrics.result(),
+                        val_acc_metrics.result()*100))
 
-def frange(x, y, jump):
-    while x < y:
-        yield x
-        x += jump
 
-EbNodB_range = list(frange(-4,8.5,0.5))
-ber = [None]*len(EbNodB_range)
-for n in range(0,len(EbNodB_range)):
-    EbNo=10.0**(EbNodB_range[n]/10.0)
-    noise_std = np.sqrt(1/(2*rate*EbNo))
-    noise_mean = 0
-    no_errors = 0
-    nn = N
-    noise = noise_std * np.random.randn(nn,n_channel)
-    encoded_signal = autoencoder.encoder.predict(test_data) 
-    final_signal = encoded_signal + noise
-    pred_final_signal =  autoencoder.decoder.predict(final_signal)
-    pred_output = np.argmax(pred_final_signal,axis=1)
-    no_errors = (pred_output != test_label)
-    no_errors =  no_errors.astype(int).sum()
-    ber[n] = no_errors / nn 
-    print ('SNR:',EbNodB_range[n],'BER:',ber[n])
 
-plt.plot(EbNodB_range, ber, 'bo',label='Autoencoder(7,4)')
-#plt.plot(list(EbNodB_range), ber_theory, 'ro-',label='BPSK BER')
-plt.yscale('log')
-plt.xlabel('SNR Range')
-plt.ylabel('Block Error Rate')
-plt.grid()
-plt.legend(loc='upper right',ncol = 1)
-plt.savefig('AutoEncoder_7_4_BER_matplotlib')
+if n_channel == 2:
+    scatter_plot = []
+    for i in range(0,M):
+        temp = np.zeros(M)
+        temp[i] = 1
+        output = autoencoder.encoder.predict(np.expand_dims(temp,axis=0))
+        encoded_signal  = np.sqrt(n_channel) * tf.math.l2_normalize(output)
+
+        scatter_plot.append(encoded_signal)
+        
+    # ploting constellation diagram
+    scatter_plot = np.array(scatter_plot).reshape(-1, 2)
+    plt.scatter(scatter_plot[:,0],scatter_plot[:,1])
+    plt.axis((-2,2,-2,2))
+    plt.grid()
+    plt.xlabel('Q Axis')
+    plt.ylabel('I Axis')
+    plt.show()
+    plt.savefig('AutoEncoder(2,2)_Constellations.png')
+
+
+else:
+    N = 20000
+    test_label = np.random.randint(M,size=N)
+    test_data = []
+
+    for i in test_label:
+        temp = np.zeros(M)
+        temp[i] = 1
+        test_data.append(temp)
+        
+    test_data = np.array(test_data)
+
+    def frange(x, y, jump):
+        while x < y:
+            yield x
+            x += jump
+
+    EbNodB_range = list(frange(-4,8.5,0.5))
+    ber = [None]*len(EbNodB_range)
+    for n in range(0,len(EbNodB_range)):
+        errors_sum = 0
+
+        EbNo=10.0**(EbNodB_range[n]/10.0)
+        noise_std = np.sqrt(1/(2*rate*EbNo))
+
+        encoded_signal = autoencoder.encoder.predict(test_data) 
+        encoded_signal  = np.sqrt(n_channel)*tf.math.l2_normalize(encoded_signal, axis=-1)
+
+        noise = noise_std * np.random.randn(encoded_signal.shape[0], n_channel)
+        channel_output = encoded_signal + noise
+
+        pred_final_signal =  autoencoder.decoder.predict(channel_output)
+        pred_output = np.argmax(pred_final_signal,axis=1)
+
+        errors = (pred_output != test_label)
+        errors_sum =  errors.astype(int).sum()
+
+        ber[n] = errors_sum / N
+        print ('SNR:',EbNodB_range[n],'BER:',ber[n])
+
+    plt.plot(EbNodB_range, ber, 'bo',label='Autoencoder(7,4)')
+    #plt.plot(list(EbNodB_range), ber_theory, 'ro-',label='BPSK BER')
+    plt.yscale('log')
+    plt.xlabel('SNR Range')
+    plt.ylabel('Block Error Rate')
+    plt.grid()
+    plt.legend(loc='upper right',ncol = 1)
+    plt.savefig('BLER versus Eb/No for (7,4) AutoEncoder.png')
